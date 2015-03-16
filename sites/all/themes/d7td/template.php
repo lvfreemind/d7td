ab0e98dbb5541c62c86f6ef7e225d4904ece69a3
lvorhis <linh.vorhis@syneron.com>
2015-03-16 02:24:37 -0700

    theming lesson2.

---
+++ sites/all/themes/d7td/template.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Returns HTML for a recent node to be displayed in the recent content block.
+ *
+ * @param $variables
+ *   An associative array containing:
+ *   - node: A node object.
+ *
+ * @ingroup themeable
+ */
+function d7td_node_recent_content($variables) {
+  $node = $variables['node'];
+
+  $output = '<div class="node-title">';
+  $output .= l($node->title, 'node/' . $node->nid);
+  $output .= theme('mark', array('type' => node_mark($node->nid, $node->changed)));
+  $output .= '</div><div class="node-author">';
+  $account = user_load($node->uid);
+  $output .= theme('username', array('account' => $account));
+  $output .= '</div>';
+  $output .= '<div class="node-created">'; 
+  $output .= format_date($node->created);
+  $output .= '</div>';
+
+  return $output;
+}
+
+function d7td_mark($variables) {
+  $type = $variables['type'];
+  global $user;
+  if ($user->uid) {
+    if ($type == MARK_NEW) {
+      return ' <span class="marker">' . t('**') . '</span>';
+    }
+    elseif ($type == MARK_UPDATED) {
+      return ' <span class="marker">' . t('*') . '</span>';
+    }
+  }
+}
+
+function d7td_preprocess_username(&$variables) {
+    if (!empty($variables['account']->mail)) {
+    $variables ['extra'] .= '(' . $variables['account']->mail . ')';
+    }
+$variables['link_attribute']['rel'] = 'nofollow';
+}
+
+function d7td_process_username(&$variables) {
+    $variables ['extra'] = str_replace('@','@NOSPAM.', $variables['extra']);
+    }
